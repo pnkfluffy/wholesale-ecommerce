710848b82bf6c52db74ee88a09ad8c4cb35e12af
ReverendEd <tysehr37@gmail.com>
2020-07-06 13:55:58 -0700

    fixed dev redirection

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -5,7 +5,7 @@ const passport = require('../modules/passport')
 const { rejectUnauthenticated } = require('../modules/authentication-middleware')
 
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
-	res.redirect('/')
+	res.redirect((process.env.NODE_ENV === 'dev') ? 'http://localhost:3000/' : '/')
 })
 
 router.get('/login/google', passport.authenticate('google', {

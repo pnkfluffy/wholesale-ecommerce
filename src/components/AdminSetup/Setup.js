9cac901867aef7b10866a543ef9720a854750277
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2020-07-07 13:01:07 -0700

    cannot find router reducer bug

--- src/components/AdminSetup/Setup.js
+++ src/components/AdminSetup/Setup.js
@@ -5,14 +5,17 @@ import { UserList} from './MenuItems/Users';
 import { PostList, PostEdit, PostCreate} from './MenuItems/Posts';
 import Dashboard from './MenuItems/Dashboard';
 import authProvider from './authProvider';
+import createHistory from 'history/createBrowserHistory';
 
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
+const history = createHistory();
+
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const AdminSetup = () =>( 
-<Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+<Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} history={history}>
   <Resource name="users" list={UserList} icon={UserIcon}/>
   <Resource name="posts" list={PostList} edit={PostEdit}  create={PostCreate} icon={PostIcon}/>
     </Admin>

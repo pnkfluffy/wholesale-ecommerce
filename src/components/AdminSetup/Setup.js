b781761fc861ee8d62d81c312cae68811d2edadb
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2020-07-07 14:41:04 -0700

    old version

--- src/components/AdminSetup/Setup.js
+++ src/components/AdminSetup/Setup.js
@@ -7,26 +7,25 @@ import defaultMessages from 'ra-language-english';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 
 import createAdminStore from './createAdminStore';
-import messages from './i18n';
+// import messages from './i18n';
 import jsonServerProvider from 'ra-data-json-server';
 
 import { UserList} from './MenuItems/Users';
 import { PostList, PostEdit, PostCreate} from './MenuItems/Posts';
 import Dashboard from './MenuItems/Dashboard';
-import authProvider from './authProvider';
 import createHistory from 'history/createBrowserHistory';
 
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
-const dataProvider = restProvider('https://jsonplaceholder.typicode.com');
-const authProvider = () => Promise.resolve();
-const i18nProvider = polyglotI18nProvider(locale => {
-    if (locale !== 'en') {
-        return messages[locale];
-    }
-    return defaultMessages;
-});
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com/");
+//const authProvider = () => Promise.resolve();
+// const i18nProvider = polyglotI18nProvider(locale => {
+//     if (locale !== 'en') {
+//         return messages[locale];
+//     }
+//     return defaultMessages;
+// });
 
 const history = createHashHistory();
 
@@ -34,13 +33,13 @@ const history = createHashHistory();
 const AdminSetup = () =>( 
 <Provider
         store={createAdminStore({
-            authProvider,
+          //  authProvider,
             dataProvider,
             history,
         })}
     >
         <Admin
-            authProvider={authProvider}
+          //  authProvider={authProvider}
             dataProvider={dataProvider}
             history={history}
             title="My Admin"

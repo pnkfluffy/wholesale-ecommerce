4e668cd26d81ac049011d7061a93151670b016d3
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2020-07-23 16:25:23 -0700

    Ready CSS for favorite products display

--- src/components/account/favoriteProductCard.js
+++ src/components/account/favoriteProductCard.js
@@ -22,12 +22,13 @@ class favoriteProductCard extends React.Component {
       <div className='favorite_product_card'>
         <div className='favorite_product_card_image' >
           <img alt='favorite_product_image' src={productImg} onClick={this.goToProduct} />
+          </div>
+        <div className='favorite_product_name'>{this.props.product.name}
+        <div className='favorite_product_order_number'>returnOrderNumber</div>
         </div>
-        <div className='favorite_product_name'>{this.props.product.name}</div>
-        <div className='favorite_product_button_container'>
-        <button className='favorite_product_button'>Go to Product</button>
-        <button className='favorite_product_button'>Order again</button>
-        </div>
+        <span className='empty_space'></span>
+        <button className='favorite_product_button' onClick={this.goToProduct} >Go to Product</button>
+        <div></div>
       </div>
     )
   }

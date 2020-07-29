4226753ec72463b06ab5f0d102f69be33a3d1e6e
Jackson F <jacksonfelty@gmail.com>
2020-07-29 14:11:49 -0600

    styled payment information area

--- src/components/cart/gcFillInfo.js
+++ src/components/cart/gcFillInfo.js
@@ -1,103 +1,118 @@
-import React from "react";
-import { connect } from "react-redux";
-import axios from "axios";
-import InputField from "../reuseable/InputField";
-import {GreenButton} from "../reuseable/materialButtons";
+import React from 'react'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import InputField from '../reuseable/InputField'
+import { GreenButton } from '../reuseable/materialButtons'
 
-const mapStateToProps = (state) => ({
-  state: state.reducer,
-});
+const mapStateToProps = state => ({
+  state: state.reducer
+})
 
 class GCFillInfo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            newClientName: "",
-            newClientLastName: "",
-            newClientEmail: "",
-            newClientAddr: "",
-            newClientCity: "",
-            newClientPostalCode: "",
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      newClientName: '',
+      newClientLastName: '',
+      newClientEmail: '',
+      newClientAddr: '',
+      newClientCity: '',
+      newClientPostalCode: ''
     }
+  }
 
-    addClients = e => {
-        e.preventDefault();
-        axios.post("/gc/addClient", this.state)
-            .then(res => {
-                console.log(res.data);
-                /*set token to finish payment*/
-                const token = res.data.token;
-                localStorage.setItem("gc", token);
-                window.open(res.data.url, "_self");
-            })
-            .catch(err => {
-                    console.log(err)
-                }
-            );
-    }
+  addClients = e => {
+    e.preventDefault()
+    axios
+      .post('/gc/addClient', this.state)
+      .then(res => {
+        console.log(res.data)
+        /*set token to finish payment*/
+        const token = res.data.token
+        localStorage.setItem('gc', token)
+        window.open(res.data.url, '_self')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
-    onChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-    render() {
-        return (
-            <div className="gcpay_form_area">
-                <h1>Payment Information</h1>
-                <form noValidate className="gc_form"
-                                 onSubmit={this.addClients}>
-                    <InputField widthCSS="gc_input"
-                                title="First Name"
-                                name="newClientName"
-                                value={this.state.newClientName}
-                                type="text" changeField={this.onChange}
-                                placeholder="" />
-                    <InputField widthCSS="gc_input"
-                                title="Family Name"
-                                name="newClientLastName"
-                                value={this.state.newClientLastName}
-                                type="text" changeField={this.onChange}
-                                placeholder="" />
-                    <InputField widthCSS="gc_input"
-                                title="Email"
-                                name="newClientEmail"
-                                value={this.state.newClientEmail}
-                                type="email" changeField={this.onChange}
-                                placeholder="" />
-                    <InputField widthCSS="gc_input"
-                                title="Address"
-                                name="newClientAddr"
-                                value={this.state.newClientAddr}
-                                type="text" changeField={this.onChange}
-                                placeholder="" />
-                    <InputField widthCSS="gc_input"
-                                title="City"
-                                name="newClientCity"
-                                value={this.state.newClientCity}
-                                type="text" changeField={this.onChange}
-                                placeholder="" />
-                    <InputField widthCSS="gc_input"
-                                title="Postal Code"
-                                name="newClientPostalCode"
-                                value={this.state.newClientPostalCode}
-                                type="text" changeField={this.onChange}
-                                placeholder="" />
-                </form>
-                <div className='cart_button_area'>
-                    <GreenButton
-                        variant='contained'
-                        className='checkout_button'
-                        onClick={this.addClients}
-                    >
-                        CREATE ACCOUNT: ${this.props.total}
-                    </GreenButton>
-                </div>
-            </div>
-        );
-        }
+  render () {
+    return (
+      <div className='gcpay_form_area'>
+        <div className='payment_header'>Payment Information</div>
+        <form noValidate className='gc_form' onSubmit={this.addClients}>
+          <InputField
+            widthCSS='gc_input'
+            title='First Name'
+            name='newClientName'
+            value={this.state.newClientName}
+            type='text'
+            changeField={this.onChange}
+            placeholder=''
+          />
+          <InputField
+            widthCSS='gc_input'
+            title='Last '
+            name='newClientLastName'
+            value={this.state.newClientLastName}
+            type='text'
+            changeField={this.onChange}
+            placeholder=''
+          />
+          <InputField
+            widthCSS='gc_input'
+            title='Email'
+            name='newClientEmail'
+            value={this.state.newClientEmail}
+            type='email'
+            changeField={this.onChange}
+            placeholder=''
+          />
+          <InputField
+            widthCSS='gc_input'
+            title='Address'
+            name='newClientAddr'
+            value={this.state.newClientAddr}
+            type='text'
+            changeField={this.onChange}
+            placeholder=''
+          />
+          <InputField
+            widthCSS='gc_input'
+            title='City'
+            name='newClientCity'
+            value={this.state.newClientCity}
+            type='text'
+            changeField={this.onChange}
+            placeholder=''
+          />
+          <InputField
+            widthCSS='gc_input'
+            title='Postal Code'
+            name='newClientPostalCode'
+            value={this.state.newClientPostalCode}
+            type='text'
+            changeField={this.onChange}
+            placeholder=''
+          />
+        </form>
+          <GreenButton
+            variant='contained'
+            className='checkout_button'
+            onClick={this.addClients}
+          >
+            CHECK OUT: ${this.props.total}
+          </GreenButton>
+      </div>
+    )
+  }
 }
 
-export default connect(mapStateToProps)(GCFillInfo);
+export default connect(mapStateToProps)(GCFillInfo)

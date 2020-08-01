66b431ee4433c61271552c1b39927b1c7cac7494
marina-mar <marinamacc@hotmail.com>
2020-08-01 04:20:49 -0300

    order history

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import FavoriteProductCard from './favoriteProductCard'
-
+import OrderHistory from '../orderHistory/orderHistory'
 const mapStateToProps = state => ({
     state: state.reducer
 })
@@ -37,8 +37,9 @@ class Account extends React.Component {
       <div>
         <div className="page_header">Account</div>
         <div className="account_container">
-        <div className="section_container" onClick={this.printPayments}>
+        <div className="section_container">
             Order History
+            <OrderHistory />
         </div>
         <div className="section_container">
         <div>Favorites</div>

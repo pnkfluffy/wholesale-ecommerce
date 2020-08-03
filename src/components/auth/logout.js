29d223337a1a7e94bf367fc90a45ffe7945da186
ReverendEd <tysehr37@gmail.com>
2020-08-03 10:25:19 -0700

    swal on logout

--- src/components/auth/logout.js
+++ src/components/auth/logout.js
@@ -1,16 +1,30 @@
 import React from "react";
 import axios from "axios";
 import { connect } from "react-redux";
+import Swal from 'sweetalert2'
 
 const mapStateToProps = (state) => ({
   state: state.reducer,
 });
 
 class Logout extends React.Component {
-  componentDidMount() {
-    axios.get("/auth/logout").then((res) => {
-      window.location.href = "/";
-    });
+
+  componentDidMount(){
+    Swal.fire({
+      title: `Logout?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#59BA47',
+    }).then(res => {
+      if (res.value) {
+        axios.get("/auth/logout").then((res) => {
+          window.location.href = "/";
+        });
+      }
+      else {
+        this.props.history.push('/')
+      }
+    })
   }
 
   render() {

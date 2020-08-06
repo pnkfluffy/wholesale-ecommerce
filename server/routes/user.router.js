cb4ee26f2089373971496e5a8b1201f2cd0f0a25
marina-mar <marinamacc@hotmail.com>
2020-08-06 01:59:37 -0300

    cart, favorites and orders check for deleted products

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -41,8 +41,9 @@ router.get('/favorites', rejectUnauthenticated, async (req, res) => {
             }
           })
           .catch(err => {
-              console.log(err)
+            console.log(err)
             res.status(500).send("couldn't get favorites")
+            return ;
           })
     }
   }

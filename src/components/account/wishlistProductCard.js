375ac7e483c2697b233a4cd27e064cf6ae64bfb6
ReverendEd <tysehr37@gmail.com>
2020-08-11 13:44:05 -0700

    added errors to wishlist handling

--- src/components/account/wishlistProductCard.js
+++ src/components/account/wishlistProductCard.js
@@ -17,29 +17,51 @@ class wishlistProductCard extends React.Component {
   }
 
   removeFromWishlist = () => {
-      this.props.dispatch({
-        type: 'DELETE_WISHLIST',
-        payload: this.props.product._id
-      })
-    }
+    this.props.dispatch({
+      type: 'DELETE_WISHLIST',
+      payload: this.props.id
+    })
+  }
 
-  render () {
-    const image = (this.props.images.length && this.props.images[0].url) ? this.props.images[0].url : blank_image;
+  render() {
     let wishlist_CancelIcon = {
       width: '1.7vw',
       height: '1.7vw',
-      };
+    };
+
+    if (this.props.product === null) {
+      return (
+        <div className='wishlist_product_card_container'>
+          <div className='wishlist_product_card'>
+            <div className='wishlist_product_card_image_container'>
+            </div>
+            <div className='wishlist_product_name'>
+              Product Unavailable
+              {/* <div className='favorite_product_order_number'>
+              returnOrderNumber
+            </div> */}
+            </div>
+            <span className='wishlist_empty_space'></span>
+            <div className='remove_button_container'>
+              <CancelIcon onClick={this.removeFromWishlist} style={wishlist_CancelIcon} />
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    const image = (this.props.images.length && this.props.images[0].url) ? this.props.images[0].url : blank_image;
 
     return (
       <div className='wishlist_product_card_container'>
         <div className='wishlist_product_card'>
           <div className='wishlist_product_card_image_container'>
             <img
-            className='wishlist_product_image'
-            alt='product_image'
-            src={image}
-            onClick={this.goToProduct}
-          />
+              className='wishlist_product_image'
+              alt='product_image'
+              src={image}
+              onClick={this.goToProduct}
+            />
           </div>
           <div className='wishlist_product_name'>
             {this.props.product.name}
@@ -49,14 +71,14 @@ class wishlistProductCard extends React.Component {
           </div>
           <span className='wishlist_empty_space'></span>
           <GreenButton
-          variant='contained'
-          className='wishlist_product_button'
-          onClick={this.goToProduct}
-        > Go to Product
+            variant='contained'
+            className='wishlist_product_button'
+            onClick={this.goToProduct}
+          > Go to Product
         </GreenButton>
           <div className='remove_button_container'>
-            <CancelIcon onClick={this.removeFromWishlist} style={wishlist_CancelIcon}/>
-            </div>
+            <CancelIcon onClick={this.removeFromWishlist} style={wishlist_CancelIcon} />
+          </div>
         </div>
       </div>
     )

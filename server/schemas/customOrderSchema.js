0947092a4b63d540979b57fe8a3b3065420f9ec1
ReverendEd <tysehr37@gmail.com>
2020-08-11 16:55:30 -0700

    yes

--- server/schemas/customOrderSchema.js
+++ server/schemas/customOrderSchema.js
@@ -3,43 +3,43 @@ const { string } = require("prop-types");
 const Schema = mongoose.Schema;
 
 const CustomOrderSchema = new Schema({
-    employee: {
-        type: Schema.Types.ObjectId,
-        ref: "user"
-    },
-    user: {
+  employee: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  standardPrice: {
+    type: Number
+  },
+  price: {
+    type: Number
+  },
+  products: [
+    {
+      product: {
         type: String,
-        required: true
+        ref: "product",
+      },
+      quantity: {
+        type: Number,
+      },
     },
-    name: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true
-    },
-    description: {
-      type: String
-    },
-    standardPrice: {
-      type: Number
-    },
-    price: {
-      type: Number
-    },
-    products: [
-        {
-          product: {
-            type: String,
-            ref: "product",
-          },
-          quantity: {
-            type: Number,
-          },
-        },
-    ],
-    date: {
-      type: Date,
-      default: Date.now
-    }
-  })
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
 
-  module.exports = Custom = mongoose.model("custom", CustomOrderSchema);
+module.exports = Custom = mongoose.model("custom", CustomOrderSchema);
